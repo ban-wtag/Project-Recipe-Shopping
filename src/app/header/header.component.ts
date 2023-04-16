a11fe6ce6f051d40b76a54b6a3c833403ccb940c
Bushra Ankhari <bushra.ankhari@welldev.iocleargit config --global user.email bushra.ankhari@welldev.io>
2023-04-16 19:34:49 +0600

    first shopping

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import 'jquery/dist/jquery.min.js';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent {
+}
