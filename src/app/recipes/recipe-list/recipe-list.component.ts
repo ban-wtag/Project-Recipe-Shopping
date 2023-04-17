3d007d8ebf199bf73fab1814939a8f1571fdb1e3
Bushra Ankhari <bushra.ankhari@welldev.iocleargit config --global user.email bushra.ankhari@welldev.io>
2023-04-17 11:48:18 +0600

    first stage done the basics Project

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,6 +12,11 @@ export class RecipeListComponent {
       'This is simply a test',
       'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505'
     ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505'
+    )
   ];
   constructor() {}
 }
